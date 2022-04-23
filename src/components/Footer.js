65cdc587c3a3f12a884534b9cb4ef0fd8b6b7d9e
Kaung Zaw Hein <kaungzawhein1963@gmail.com>
2022-04-23 23:24:58 +0630

    basic design init

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p> &copy; 2021 IndianFooood.com</p>
+    </div>
+  );
+}
+
+export default Footer;
